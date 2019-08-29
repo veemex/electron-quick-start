a247a4baa4ba5977b050d226e09256298cb38dc8
veemex <tcm.mihai@gmail.com>
2019-08-29 13:12:50 +0300

    bug exemple

--- renderer.js
+++ renderer.js
@@ -1,3 +1,40 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+// In the renderer process.
+const { desktopCapturer } = require('electron');
+
+desktopCapturer.getSources({ types: ['window', 'screen'] }).then(async sources => {
+	for (const source of sources) {
+		console.log(sources);
+		if (source.name === 'Entire screen') {
+			try {
+				const stream = await navigator.mediaDevices.getUserMedia({
+					audio: false,
+					video: {
+						mandatory: {
+							chromeMediaSource: 'desktop',
+							chromeMediaSourceId: source.id,
+							minWidth: 1280,
+							maxWidth: 1280,
+							minHeight: 720,
+							maxHeight: 720,
+						},
+					},
+				});
+				handleStream(stream);
+				console.log(stream);
+			} catch (e) {
+				handleError(e);
+			}
+			return;
+		}
+	}
+});
+
+function handleStream(stream) {
+	const video = document.querySelector('video');
+	video.srcObject = stream;
+	video.onloadedmetadata = e => video.play();
+}
+
+function handleError(e) {
+	console.log(e);
+}
